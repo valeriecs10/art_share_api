1e625ec99160aa0dffd6907c470b1b0ad9c2b43b
Valerie Campbell <valeriecs10@gmail.com>
2020-11-29 14:46:25 -0800

    new rails project... added routes for users

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # resources :users
+  # get, post, patch, put, delete
+
+  # index, show, destroy, update, create
+  get 'users', to: 'users#index', as: 'users'
+  post 'users', to: 'users#create'
+  get 'users/new', to: 'users#new', as: 'new_user'
+  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+  get 'users/:id', to: 'users#show', as: 'user'
+  patch 'users/:id', to: 'users#update'
+  put 'users/:id', to: 'users#update'
+  delete 'users/:id', to: 'users#destroy'
+
+end
