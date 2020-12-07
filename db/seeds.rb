30d4b1252462bf445afd648c5d4aa3b4c8485612
Valerie Campbell <valeriecs10@gmail.com>
2020-12-06 19:05:47 -0800

    created initial db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'Val')
+User.create(username: 'Spongebob')
+User.create(username: 'Greg')
+
+Artwork.create(title: 'Pretty Trees', image_url: 'images/pretty_tree.jpg', artist_id: 1)
+Artwork.create(title: 'Gary Portrait', image_url: 'images/gary_portrait.jpg', artist_id: 2)
+Artwork.create(title: 'Fruit Bowl', image_url: 'images/fruit_bowl.jpg', artist_id: 1)
+
+ArtworkShare.create(viewer_id: 3, artwork_id: 1)
+ArtworkShare.create(viewer_id: 3, artwork_id: 2)
+ArtworkShare.create(viewer_id: 1, artwork_id: 3)
+ArtworkShare.create(viewer_id: 1, artwork_id: 2)
