652d1a52351a6215bf8087c7a565dd4375769627
Valerie Campbell <valeriecs10@gmail.com>
2020-12-06 19:06:55 -0800

    added Artwork model w validations and associations

---
+++ app/models/artwork.rb
@@ -0,0 +1,21 @@
+class Artwork < ApplicationRecord
+  validates :title, :image_url, presence: true
+  validates :title, uniqueness: { 
+    scope: :artist_id,
+    message: "cannot be duplicated by same artist"
+  }
+
+  belongs_to :artist,
+    class_name: :User,
+    foreign_key: :artist_id,
+    primary_key: :id
+
+  has_many :shares,
+    class_name: :ArtworkShare,
+    foreign_key: :artwork_id,
+    primary_key: :id
+
+  has_many :shared_viewers,
+    through: :shares,
+    source: :viewer
+end
