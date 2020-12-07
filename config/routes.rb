a54cd46faccbaac55fbaf048f32bd2769dca2806
Valerie Campbell <valeriecs10@gmail.com>
2020-12-06 22:24:13 -0800

    fixed routes, added nested artworks route

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  # resources :users
   # get, post, patch, put, delete
-
   # index, show, destroy, update, create
-  get 'users', to: 'users#index', as: 'users'
-  post 'users', to: 'users#create'
-  get 'users/new', to: 'users#new', as: 'new_user'
-  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
-  get 'users/:id', to: 'users#show', as: 'user'
-  patch 'users/:id', to: 'users#update'
-  put 'users/:id', to: 'users#update'
-  delete 'users/:id', to: 'users#destroy'
-
+  
+  resources :users, only: [:index, :show, :create, :destroy, :update] do
+    resources :artworks, only: :index
+  end
+  resources :artworks, only: [:show, :create, :update, :destroy]
+  resources :artwork_shares, only: [:create, :destroy]
+  
 end
