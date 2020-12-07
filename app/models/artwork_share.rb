02772a9846088ca2b11e0df3fc7e8251e2cbf627
Valerie Campbell <valeriecs10@gmail.com>
2020-12-06 22:17:55 -0800

    added not_view_own_artwork validation to artwork_share model

--- app/models/artwork_share.rb
+++ app/models/artwork_share.rb
@@ -4,6 +4,7 @@ class ArtworkShare < ApplicationRecord
     scope: :viewer_id,
     message: "can't be shared with the same viewer twice"
   }
+  validate :not_view_own_artwork
 
   belongs_to :artwork,
     class_name: :Artwork,
@@ -14,4 +15,14 @@ class ArtworkShare < ApplicationRecord
     class_name: :User,
     foreign_key: :viewer_id,
     primary_key: :id 
+
+  def viewers_own_artwork?
+    viewer.id == artwork.artist_id
+  end
+
+  def not_view_own_artwork
+    if viewers_own_artwork?
+      errors[:artwork] << "can't be shared with its own artist"
+    end
+  end
 end
