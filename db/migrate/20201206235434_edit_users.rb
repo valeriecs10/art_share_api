ebacd444759100902485988c9f6681d8c62df56a
Valerie Campbell <valeriecs10@gmail.com>
2020-12-06 16:00:52 -0800

    changed columns in users table

---
+++ db/migrate/20201206235434_edit_users.rb
@@ -0,0 +1,7 @@
+class EditUsers < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :name
+    remove_column :users, :email
+    add_column :users, :username, :string, null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_201428) do
+ActiveRecord::Schema.define(version: 2020_12_06_235434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name", "email"], name: "index_users_on_name_and_email"
+    t.string "username", null: false
   end
 
 end
