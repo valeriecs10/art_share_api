4749964a4d4133a1a2259884b0978f69a1831b9c
Valerie Campbell <valeriecs10@gmail.com>
2020-12-06 19:03:47 -0800

    added associations :artworks, :received_shares, :viewed_artworks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,17 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
 
+  has_many :artworks,
+    class_name: :Artwork,
+    foreign_key: :artist_id,
+    primary_key: :id
 
+  has_many :received_shares,
+    class_name: :ArtworkShare,
+    foreign_key: :viewer_id,
+    primary_key: :id
+
+  has_many :viewed_artworks,
+    through: :received_shares,
+    source: :artwork
 end
